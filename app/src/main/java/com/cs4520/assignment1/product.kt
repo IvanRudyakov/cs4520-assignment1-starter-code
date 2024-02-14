49839308f442b50f92041842dc939c4630d5e2be
Ivan Rudyakov <rudyakov.i@northeastern.edu>
2024-02-14 07:57:06 -0500

    changed file name

--- app/src/main/java/com/cs4520/assignment1/product.kt
+++ app/src/main/java/com/cs4520/assignment1/product.kt
@@ -4,4 +4,4 @@ sealed class Product(val name: String, val price: Int, val expiryDate: String?);
 
 class Equipment(name: String, price: Int, exp: String?): Product(name, price, exp);
 
-class Food(name: String, price: Int, exp: String?): Product(name, price, exp);
+class Food(name: String, price: Int, exp: String?): Product(name, price, exp); 
